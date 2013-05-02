d261f21606dd19538719f709fb12deeb6c51fdf7
MarekDudek <marek.dudek@gmail.com>
2013-05-02 18:45:30 +0200

    building JSON tested

---
+++ src/HAT/Person.hs
@@ -0,0 +1,7 @@
+module HAT.Person where
+
+
+data Person = Person {
+  name    :: String,
+  surname :: String
+} deriving (Show)
--- test/HAT/JSONTests.hs
+++ test/HAT/JSONTests.hs
@@ -5,13 +5,35 @@ import Test.Framework.Providers.QuickCheck2
 import Test.Framework.Providers.HUnit
 import Test.HUnit
 
+import Text.JSON
+
+import HAT.Person
+
+
+person = Person "Marek" "Dudek"
+
+personJsonObj :: JSValue
+personJsonObj = makeObj [
+    ("name",    JSString $ toJSString $ name person),
+    ("surname", JSString $ toJSString $ surname person)
+  ]
+
+personJsonStr :: String
+personJsonStr = encode personJsonObj
+
+personJsonStrictStr = encodeStrict personJsonObj
+
 tests = [
-    testGroup "Group name" [
-      testCase "Case name" (
-        assertBool "" True
+    testGroup "JSON encoding" [
+      testCase "Encoding" (
+        assertEqual'  "{\"name\":\"Marek\",\"surname\":\"Dudek\"}"  personJsonStr 
+      ),
+      testCase "Strict encoding" (
+        assertEqual'  "{\"name\":\"Marek\",\"surname\":\"Dudek\"}"  personJsonStrictStr 
       )
     ]
   ]
  
 main = defaultMain tests
 
+assertEqual' expected actual = assertEqual "" expected actual
