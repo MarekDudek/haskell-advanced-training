5c5526b67a150d9788de80e9a028538bbd60cb8e
MarekDudek <marek.dudek@gmail.com>
2013-05-08 21:15:56 +0200

    options with required and optional argument read from command line

--- src/Main.hs
+++ src/Main.hs
@@ -3,47 +3,33 @@ module Main where
 import System.Environment
 import System.Console.GetOpt
 
+import Data.List
 
 main = do
   putStrLn "Haskell advanced training"
-
-  args <- getArgs
-  -- putStrLn $ "Arguments: " ++ show args
-
+  args        <- getArgs
   programName <- getProgName
-  -- putStrLn $ "Program name is " ++ programName
-
-  let ( flags, nonOpts, msgs ) = getOpt RequireOrder options args
-  -- debugCommandLine flags nonOpts msgs
-  case (flags, nonOpts, msgs ) of 
-    (flags, [],      []  ) -> 
-      do 
-        putStr $ "All OK: "
-        mapM_ putStr (map show flags)
-        putStrLn ""
-    (_,     nonOpts, []  ) -> 
-      do
-        putStrLn $ "Unrecognised arguments: " ++ unwords nonOpts
-        error $ usageInfo header options
-    (_,     nonOpts, msgs) -> 
-      do 
-        putStrLn $ "Unrecognised arguments: " ++ unwords nonOpts
-        putStrLn "Error messages: " 
-        mapM_ putStr msgs
-        error $ usageInfo header options
+  let ( opts, nonOpts, msgs ) = getOpt Permute options args
+  case (opts, nonOpts, msgs ) of 
+    (opts, [],      []  ) -> do 
+      putStr $ "Options: "
+      let optionString = map show opts
+      mapM_ putStr (intersperse ", " optionString)
+      putStrLn ""
+    (_,     nonOpts, msgs) -> do 
+      putStrLn $ "Unrecognised arguments: " ++ unwords nonOpts
+      putStrLn "Error messages: " 
+      mapM_ putStrLn msgs
+      error $ usageInfo (header programName) options
   
 
 options :: [OptDescr Flag]
-options = [versionOption]
+options = [versionOption, inputOption, outputOption]
 
-header = "Usage: hat [OPTION ...]"
-data Flag = Version deriving Show
+header name = "Usage: " ++ name ++ " [OPTION ...]"
+data Flag = Version | Input String | Output (Maybe String)
+  deriving Show
 
-versionOption = Option ['v'] ["version"] (NoArg Version) "show version number"
-  
-debugCommandLine flags nonOpts msgs = do
-  putStrLn $ "#flags  : " ++ show (length flags)
-  putStrLn $ "flags   : " ++ show flags
-  putStrLn $ "non-opts: " ++ show nonOpts
-  putStrLn $ "messages: " ++ show msgs
-
+versionOption = Option ['v'] ["version"] (NoArg Version)        "show version number"
+inputOption   = Option ['i'] ["input"]   (ReqArg Input  "FILE") "input file"
+outputOption  = Option ['o'] ["output"]  (OptArg Output "FILE") "output file"
